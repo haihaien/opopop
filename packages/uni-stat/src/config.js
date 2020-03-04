f9e270bb760abccb1ada47d489c49c714bc5a6f6
yangheng <better_yh@163.com>
2020-03-04 14:43:33 +0800

    fix：行为采集

--- packages/uni-stat/src/config.js
+++ packages/uni-stat/src/config.js
@@ -2,9 +2,9 @@ import {
   version
 } from '../package.json'
 export const STAT_VERSION = version
-export const STAT_URL = 'https://tongji.dcloud.io/uni/stat'
-export const STAT_H5_URL = 'https://tongji.dcloud.io/uni/stat.gif' 
-export const STAT_KEY = 'qkTHEIegZGcL5iy3'
-export const PAGE_PVER_TIME = 1800
+export const STAT_URL = 'http://219.143.38.252:19080/yump-mgw/log/log-collector/t0001'
+export const STAT_H5_URL = 'http://219.143.38.252:19080/yump-mgw/log/log-collector/t0001'
+export const STAT_KEY = ''
+export const PAGE_PVER_TIME = 100
 export const APP_PVER_TIME = 300
 export const OPERATING_TIME = 10
